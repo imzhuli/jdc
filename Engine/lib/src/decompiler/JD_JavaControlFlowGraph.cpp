66fb8ac70268a43d70673171bbd236ceeebfdd48
zhuli <imzhuli@vip.qq.com>
2023-04-02 03:48:31 +0800

    remove fix method name

--- Engine/lib/src/decompiler/JD_JavaControlFlowGraph.cpp
+++ Engine/lib/src/decompiler/JD_JavaControlFlowGraph.cpp
@@ -1,19 +1,24 @@
 #include <jdc/decompiler/JD_JavaControlFlowGraph.hpp>
+#include <jdc/decompiler/JD_JavaMethod.hpp>
 
 namespace jdc
 {
 
 
-    std::unique_ptr<xJavaControlFlowGraph> xJavaControlFlowGraph::ParseByteCode(const xJavaClass * JavaClassPtr, const xAttributeCode & CodeAttribute)
+    std::unique_ptr<xJavaControlFlowGraph> xJavaControlFlowGraph::ParseByteCode(const xJavaMethod * JavaMethodPtr)
     {
         auto JavaControlFlowGraphUPtr = std::make_unique<xJavaControlFlowGraph>();
         auto CFGPtr = JavaControlFlowGraphUPtr.get();
 
+        auto JavaClassPtr = JavaMethodPtr->JavaClassPtr;
+        auto CodeAttributePtr = (const xAttributeCode *)GetAttributePtr(JavaMethodPtr->Converted.AttributeMap, "Code");
+        auto & CodeBinary = CodeAttributePtr->CodeBinary;
 
 
-
-
+        (void) CodeBinary;
+        (void) JavaClassPtr;
         (void) CFGPtr;
+
         return JavaControlFlowGraphUPtr;
     }
 
